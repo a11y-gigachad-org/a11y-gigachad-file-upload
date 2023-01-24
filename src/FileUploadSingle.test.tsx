e6479747b0339cce1ab9bbe418414949b7f4695c
John James <57100733+john-james-cko@users.noreply.github.com>
2023-01-24 22:35:38 +0000

    refactor(test): improve legibility

--- src/FileUploadSingle.test.tsx
+++ src/FileUploadSingle.test.tsx
@@ -5,6 +5,7 @@ import { setupServer } from "msw/node"
 import { FileUploadSingle } from "./"
 
 const server = setupServer()
+
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
